2ff9fb01509516827c60010976a7ee47391d0071
Katarzyna Kensy <zbierek@gmail.com>
2018-05-17 17:21:30 +0200

    Beautify script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function Phone(brand, price, color) {
-	this.brand = brand;
-	this.price = price;
-	this.color = color;
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
 }
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
-Phone.prototype.yourDiscount = function () {
-  console.log("Today is your lucky day and the price is " + (this.price - 100) + "!");
+Phone.prototype.yourDiscount = function() {
+    console.log("Today is your lucky day and the price is " + (this.price - 100) + "!");
 }
 var samsungGalaxyS6 = new Phone("Samsung Galaxy S6", "2500", "black");
 var iPhone6S = new Phone("Iphone 6S", "3200", "gold");
